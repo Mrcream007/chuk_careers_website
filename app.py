10eb90a9e0baf86092092605a0f78611271939ab
mrcream007 <okochachuka@gmail.com>
2025-11-18 19:29:07 +0000

    chuck

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'London, England',
+        'salary': '£33,000'
+    },
+    {
+        'id': 2,
+        'title': 'Software Developer',
+        'location': 'Aberdeen, Scotland',
+        'salary': '£37,600'
+    },
+    {
+        'id': 3,
+        'title': 'Data Scientist',
+        'location': 'New York, USA',
+        'salary': '$55,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
     #return "Hello Chuka!"
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='mrcream007')
 
 #this is to open/establish a local server with if statement 
 if __name__ == "__main__":
