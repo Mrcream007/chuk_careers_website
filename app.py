84936d4210ccc312e1b5925de3bf8803c222412e
mrcream007 <okochachuka@gmail.com>
2025-11-18 16:18:06 +0000

    Chuck

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello Chuka!"
+    #return "Hello Chuka!"
+    return render_template('home.html')
 
 #this is to open/establish a local server with if statement 
 if __name__ == "__main__":
